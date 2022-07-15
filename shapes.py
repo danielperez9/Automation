9063df9f78e0bda4e87dd95ff33d6d551cd153a8
danielperez9 <108952044+danielperez9@users.noreply.github.com>
2022-07-15 13:57:03 -0400

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -6,7 +6,7 @@ import copy
 import shutil
 import time
 
-
+#checking for the correct directory
 def right_directory():
     current_directory=os.getcwd()
     if current_directory !='D:\Resume':
@@ -16,23 +16,26 @@ def right_directory():
         filesort()
 
 new_directory_name=input("Enter a name of the new Folder: ")
+
 def filesort():
     
-    search_directory=os.getcwd()        
+    search_directory=os.getcwd()    #go to the directory stated earlier    
     
-    os.mkdir(new_directory_name)
-    new_directory=os.path.join(search_directory,new_directory_name)
+    os.mkdir(new_directory_name)# new directory being made
+    new_directory=os.path.join(search_directory,new_directory_name)# path for new directory made
+    # retrieving and organizing files from most recent to oldest
     file_list=os.listdir(search_directory)
     files=list(filter(os.path.isfile,file_list))
     files.sort(key=os.path.getctime,reverse=True)
-    search_date_entry=input("Enter the date in MM/DD/YYYY format: ")
     
+    search_date_entry=input("Enter the date in MM/DD/YYYY format: ")#date for file search
     
+    #comparing eah file in directory to date desired
     for index in files:
         file_time=datetime.fromtimestamp(os.path.getctime(index))
         file_time=file_time.strftime("%m/%d/%Y")
         
-
+        #copyin each file that matches date created with dsied date to new directory
         if file_time==search_date_entry:  
             shutil.copy(index,new_directory)
             
@@ -41,12 +44,14 @@ right_directory()
 
 def rename_files():
     old_dir=os.getcwd()
+    #retrieving path to directory made earlier
     new_directory=os.path.join(old_dir,new_directory_name)
     os.chdir(new_directory)
     print (new_directory)
     fila=os.listdir(new_directory)
 
     number=0
+    #small loop to rename each file in the new directory
     for f in fila:
         print(f)
         filetime=datetime.fromtimestamp(os.path.getctime(f))
