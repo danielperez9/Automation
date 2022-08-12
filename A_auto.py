d3a96a1d9b8d125b77fd7d4641d6c19ad601831f
danielperez9 <108952044+danielperez9@users.noreply.github.com>
2022-08-12 16:16:15 -0400

    Update A_auto.py

--- A_auto.py
+++ A_auto.py
@@ -1,3 +1,8 @@
+#When the cript is run it will start and ask you to enter a date,
+# the date is the date in which the files have been made.
+# After it will ask you to enter a naem for the folder in which the files from that day will be placed in.
+#this code will not edit the files, just copy them in the new directory and rename them
+
 from copy import copy
 from datetime import *
 import os
@@ -25,17 +30,20 @@ def filesort():
     
     os.mkdir(new_directory_name)
     new_directory=os.path.join(search_directory,new_directory_name)
+    #makeing a new direcotry with name inputed in the diretciry win which files are found
+    
+    #listing files by most recently made
     file_list=os.listdir(search_directory)
     files=list(filter(os.path.isfile,file_list))
     files.sort(key=os.path.getctime,reverse=True)
     search_date_entry=input("Enter the date in MM/DD/YYYY format: ")
     
-    
+    #comparing file date of creation to the date inputed
     for index in files:
         file_time=datetime.fromtimestamp(os.path.getctime(index))
         file_time=file_time.strftime("%m/%d/%Y")
         
-
+#if the dates match they will be copied to the new directory made
         if file_time==search_date_entry:  
             shutil.copy(index,new_directory)
             
@@ -53,23 +61,9 @@ def rename_files():
     for f in fila:
         filetime=datetime.fromtimestamp(os.path.getctime(f))
         filetime=filetime.strftime("%Y-%m-%d")
+        #renaming base on direcoty name, adding a sequential number and adding the date
         filenames = new_directory_name +'_'+str(number+1)+'_'+filetime+".txt"
         os.rename(f,filenames)
         number+=1
 
 rename_files()
-#accesing a differnt direcotry and editing a 
-# single line in multiple text files
-def edit_file():
-    old_dir=os.getcwd()
-
-    fila=os.listdir(old_dir)
-    for f in fila:
-        with open(f,"r") as file:    
-            data=file.readlines()
-            # the number determines which line * python starts at 0
-            data[1]="I need to get my story straight\n"
-        with open(f,'w') as file:
-            file.writelines(data)
-
-#edit_file()
